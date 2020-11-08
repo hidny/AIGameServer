58307ec989841e2c6acb08057126808c71c1a58d
hidny <mtardibuono@gmail.com>
2020-11-08 17:12:32 -0500

    Added debug messages and fixed some kind of bug with miniserver

--- src/severalClientProject/MiniServer.java
+++ src/severalClientProject/MiniServer.java
@@ -19,34 +19,32 @@ import java.net.Socket;
 //Add ability to start holdem!
 //keep going though code and adding functions.
 
-public class MiniServer extends Thread{
+public class MiniServer extends Thread {
 
 	public static final String EOC = "Goodbye.";
-	
-    private Object lockSubmitQueryFromClientMiniServ = new Object();
-    private Object lockSendMessageToClientMiniServ = new Object();
+
+	private Object lockSubmitQueryFromClientMiniServ = new Object();
+	private Object lockSendMessageToClientMiniServ = new Object();
 
 	public static final String EOT = "**end of transmission**";
-	
-    private Socket socket = null;
 
-    private Profile clientProfile = null;
-    
-    private BufferedReader inFromClient;
-    private DataOutputStream outToClient;
-    
-    //private String immediateResponse;
-      
-    private boolean isConnected = true;
-    
-    
-    
-    public MiniServer(Socket socket) {
-        super("MiniServer");
-        this.socket = socket;
-    }
+	private Socket socket = null;
 
-    public void run() {
+	private Profile clientProfile = null;
+
+	private BufferedReader inFromClient;
+	private DataOutputStream outToClient;
+
+	// private String immediateResponse;
+
+	private boolean isConnected = true;
+
+	public MiniServer(Socket socket) {
+		super("MiniServer");
+		this.socket = socket;
+	}
+
+	public void run() {
     	try {
     		System.out.println("Running mini server!");
 	    	
@@ -59,6 +57,12 @@ public class MiniServer extends Thread{
 				
 				System.out.println("TEST Before read in miniserver!");
 				clientSentence = inFromClient.readLine();
+				
+				if(clientSentence == null) {
+					System.out.println("ERROR: Found a null when getting message from client!");
+					break;
+				}
+				System.out.println(clientSentence);
 				System.out.println("TEST After read in miniserver!");
 				
 				if(clientSentence != null) {
@@ -89,79 +93,76 @@ public class MiniServer extends Thread{
     	isConnected = false;
     	
     }
-    
-    public void sendMessageToClient(String message)  throws IOException {
-    	synchronized(lockSendMessageToClientMiniServ) {
-	    	System.out.println("Trying to send: " + message + '\n' + EOT);
-	    	
-	    	if(message.equals("/disc")) {
-	    		isConnected = false;
-	    		return;
-	    	}
-	    	//sanitize output just in case:
-	    	while(message.endsWith("\n")) {
-	    		message = message.substring(0, message.length() - 1);
-	    	}
-	    	
-	    	
-	    	//outToClient.writeBytes(message /*+ '\n'*/ + EOT /*+ '\n'*/ + typeOfRequest);
-	    	outToClient.writeBytes(message + '\n');
-	    	outToClient.writeBytes(EOT + '\n');
-	    	System.out.println("End of write bytes!");
-    	}
-    }
-    
-   
-    
 
-    public boolean isConnectionStillOpen() {
-    	return isConnected;
-    }
-    
-    private void submitClientQuery(String query) {
-    	synchronized(lockSubmitQueryFromClientMiniServ) {
-	    	if(clientProfile == null) {
-	    		String clientName = query.trim();
-	    		
-	    		if(Server.isClientNameTaken(clientName) == false) {
-	    			System.out.println("TEST: Creating new profile: ");
-	    			this.clientProfile = Server.createNewProfile(this, clientName);
-	    		} else if(Server.isClientNameWaitingForConnection(clientName) == true) {
-	    			this.clientProfile = Server.reconnectToProfile(this, clientName);
-	    		} else {
-	    			try {
-	    				sendMessageToClient("ERROR: " + clientName + " is already online.");
-	    			} catch (IOException e) {
-	    				e.printStackTrace();
-	    			}
-	    			return;
-	    		}
-	    		
-	    		//TODO: block until profile allows the sending of msgs back to client...
-	    		while(this.clientProfile.isMiniServerConnectionOpen() == false) {
-	    			//TODO: just do a thread sleep because you're lazy...
-	    			try {
+	public void sendMessageToClient(String message) throws IOException {
+		synchronized (lockSendMessageToClientMiniServ) {
+			System.out.println("Trying to send: " + message + '\n' + EOT);
+
+			if (message.equals("/disc")) {
+				isConnected = false;
+				return;
+			}
+			// sanitize output just in case:
+			while (message.endsWith("\n")) {
+				message = message.substring(0, message.length() - 1);
+			}
+
+			// outToClient.writeBytes(message /*+ '\n'*/ + EOT /*+ '\n'*/ +
+			// typeOfRequest);
+			outToClient.writeBytes(message + '\n');
+			outToClient.writeBytes(EOT + '\n');
+			System.out.println("End of write bytes!");
+		}
+	}
+
+	public boolean isConnectionStillOpen() {
+		return isConnected;
+	}
+
+	private void submitClientQuery(String query) {
+		synchronized (lockSubmitQueryFromClientMiniServ) {
+			if (clientProfile == null) {
+				String clientName = query.trim();
+
+				if (Server.isClientNameTaken(clientName) == false) {
+					System.out.println("TEST: Creating new profile: ");
+					this.clientProfile = Server.createNewProfile(this, clientName);
+				} else if (Server.isClientNameWaitingForConnection(clientName) == true) {
+					this.clientProfile = Server.reconnectToProfile(this, clientName);
+				} else {
+					try {
+						sendMessageToClient("ERROR: " + clientName + " is already online.");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					return;
+				}
+
+				// TODO: block until profile allows the sending of msgs back to
+				// client...
+				while (this.clientProfile.isMiniServerConnectionOpen() == false) {
+					// TODO: just do a thread sleep because you're lazy...
+					try {
 						Thread.sleep(100);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-	    			System.out.println(":(");
-	    		}
-	    		
-	    		try {
-	    			sendMessageToClient("Hello, " + clientName + ". Type /help or /h for help.");
-	    		} catch (IOException e) {
-	    			System.err.println("USER that isn't logged in quit the game.");
-	    			e.printStackTrace();
-	    		}
-	    		
-	    	} else {
-	    		clientProfile.submitClientQuery(query);
-	    	}
-    	}
-    	
-    }
-    
+					System.out.println(":(");
+				}
+
+				try {
+					sendMessageToClient("Hello, " + clientName + ". Type /help or /h for help.");
+				} catch (IOException e) {
+					System.err.println("USER that isn't logged in quit the game.");
+					e.printStackTrace();
+				}
+
+			} else {
+				clientProfile.submitClientQuery(query);
+			}
+		}
+
+	}
 
 }
